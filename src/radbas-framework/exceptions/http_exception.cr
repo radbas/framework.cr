471e5f93bdcf5997944c1700bfa0a50b817cce56
Johannes Rabausch <mail@jrabausch.de>
2023-12-16 17:19:59 +0100

    Added status description as message default

--- src/radbas-framework/exceptions/http_exception.cr
+++ src/radbas-framework/exceptions/http_exception.cr
@@ -3,7 +3,7 @@ class Radbas::HttpException < Exception
 
   @status = HTTP::Status::INTERNAL_SERVER_ERROR
 
-  def initialize(@context : Context, message : String? = nil, cause : Exception? = nil)
+  def initialize(@context : Context, message : String = @status.description, cause : Exception? = nil)
     super(message, cause)
   end
 end
